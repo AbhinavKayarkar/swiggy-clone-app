01fa9b90d9c5a406d1d4a799f4c38bfc5309f44b
abhinavkayarkar <abhinavkayarkar@gmail.com>
2024-12-22 21:22:17 +0530

    content added

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const AboutUs = () => {
   return (
     <>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur commodi
-      iste, sunt explicabo quibusdam magnam placeat, ipsum iure fuga dolore,
-      quidem ipsa nobis. Doloremque, fuga nobis? Natus recusandae magni vel?
+      <p>
+        Swiggy is a new-age consumer-first organization offering an easy-to-use
+        convenience platform, accessible through a unified app
+      </p>
     </>
   );
 };
