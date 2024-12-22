d5dbe13e29f34779aa34b10cfc24a744cd70fc8f
abhinavkayarkar <abhinavkayarkar@gmail.com>
2024-12-22 21:24:39 +0530

    update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,8 +4,46 @@ const AboutUs = () => {
   return (
     <>
       <p>
-        Swiggy is a new-age consumer-first organization offering an easy-to-use
-        convenience platform, accessible through a unified app
+        In 2011, Sriharsha Majety and Nandan Reddy designed an e-commerce
+        website called Bundl to facilitate courier service and shipping within
+        India.[6] Bundl was halted in 2014 and rebranded to enter the food
+        delivery market.[7] Majety and Reddy approached Rahul Jaimini, formerly
+        with Myntra, and founded Swiggy in August 2014.[8][6][7] 
+        By 2015, Swiggy
+        expanded its food delivery operations from just Bengaluru to eight Tier
+        1 cities across India.[9][10] At the time, the food delivery sector was
+        in turmoil as several notable startups, such as Foodpanda (later
+        acquired by Ola Cabs), TinyOwl (later acquired by Zomato) and Ola Cafe
+        (later closed) were struggling.[6][7] In January 2017, Swiggy started
+        its cloud kitchen chain called "The Bowl Company".[11] In November 2017,
+        Swiggy started a kitchen incubator business called Swiggy Access,
+        opening a network of ready-to-occupy kitchens for its restaurant
+        partners.[12] By 2019, over 1,000 Swiggy Access kitchens were
+        operational, according to a TechCrunch report.[13] In mid-2018, Swiggy
+        was operational in 16 Indian cities,[14] which increased to 500 cities
+        in 2019, matching the scale of rival Zomato.[15] In early 2019, Swiggy
+        expanded into general product deliveries under the name Swiggy Stores,
+        sourcing items from local stores.[16][17][18] In September 2019, Swiggy
+        launched the instant pickup/dropoff service Swiggy Go, allowing
+        customers to send document or parcel deliveries.[19][20] In April 2020,
+        it rebranded Swiggy Go as Swiggy Genie.[21] During the COVID-19
+        pandemic, it began doorstep delivery of alcohol in the states of
+        Jharkhand, West Bengal and Odisha.[22][23] In May 2020, Swiggy laid off
+        1,100 employees during the COVID-19 pandemic.[24] The pandemic also
+        resulted in the shut down of more than three-fourths of its cloud
+        kitchens.[25] A Swiggy delivery partner swapping the battery of his
+        electric scooter A Swiggy delivery partner swapping the battery of his
+        electric scooter In August 2020, Swiggy launched its instant grocery
+        delivery service called Instamart using a network of dark
+        stores.[26][27] In early 2021, the company closed Swiggy Stores and
+        expanded its operations under Instamart.[28] In 2023, it sold Swiggy
+        Access kitchens to Kitchens@ in a share-swap deal.[29] In January 2024,
+        Swiggy laid off 400 employees, or 6% of its workforce, ahead of
+        IPO.[30][31] In April 2024, Swiggy converted itself into a public
+        limited company[32] and confidentially filed for an initial public
+        offering.[33] Swiggy launched its initial public offering (IPO) in
+        November 2024 at ₹390 per share, valuing the company at $11.3
+        billion.[34]
       </p>
     </>
   );
